bfb34f96aff3cbc61399d3a235795f35e44c2ac2
abdullah47044704-dotcom <abdullah47044704@gmail.com>
2026-03-16 00:08:50 +0600

    Refactor upload function to use let instead of const

--- public/script.js
+++ public/script.js
@@ -1,9 +1,10 @@
-async function upload(){
+function upload(){
 
-const files=document.getElementById("files").files
-const caption=document.getElementById("caption").value
+let files=document.getElementById("files").files
 
-const form=new FormData()
+let caption=document.getElementById("caption").value
+
+let form=new FormData()
 
 for(let f of files){
 
@@ -13,22 +14,29 @@ form.append("files",f)
 
 form.append("caption",caption)
 
-const xhr=new XMLHttpRequest()
+let xhr=new XMLHttpRequest()
 
 xhr.open("POST","/upload")
+
+let start=Date.now()
 
 xhr.upload.onprogress=(e)=>{
 
 let percent=Math.round((e.loaded/e.total)*100)
 
-document.getElementById("progress").innerHTML=
-"Uploading: "+percent+"%"
+let time=(Date.now()-start)/1000
+
+let speed=(e.loaded/1024/1024/time).toFixed(2)
+
+document.getElementById("progress").innerHTML="Progress: "+percent+"%"
+
+document.getElementById("speed").innerHTML="Speed: "+speed+" MB/s"
 
 }
 
 xhr.onload=()=>{
 
-const res=JSON.parse(xhr.response)
+let res=JSON.parse(xhr.response)
 
 let html=""
 
